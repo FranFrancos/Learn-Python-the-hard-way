077ac7ead961c43f8c4d48167fd66fef9ccbe4cf
Francisco Francos <fran_francos@hotmail.com>
2019-05-08 21:26:21 +0200

    ex3

---
+++ ex3.py
@@ -0,0 +1,23 @@
+print ("I will now conunt my chickens:") # print text
+
+print ("Hens", 25 + 30 / 6) # print text + math operation
+print ("Roosters", 100 - 25 * 3 % 4) # print text + math operation
+
+print ("Now I will count the eggs:") # print text
+
+print (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) # print math operation
+
+print ("Is it true that 3 + 2 < 5 - 7?") # print text
+
+print (3 + 2 < 5 - 7) # print boolean False
+
+print ("What is 3 + 2?", 3 + 2) # print text and math operation
+print ("What is 5 - 7?", 5 - 7) # print text and math operation
+
+print ("Oh, that's why it's False.") # print text
+
+print ("How about some more.") # print text
+
+print ("Is it greater?", 5 > -2) # print text and boolean True
+print ("Is it greater or equal?", 5 >= -2) # print text and boolean True
+print ("Is it less or equal?", 5 <= -2) # print text and boolean False
